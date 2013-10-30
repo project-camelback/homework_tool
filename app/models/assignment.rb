054df36528d5e5ba1d019ed00829f725bdffbb82
Logan Hasson <logan.hasson@gmail.com>
2013-10-30 08:58:13 -0400

    Grabbing avatar

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -6,6 +6,7 @@ class Assignment < Sequel::Model
     github_fork = GithubForks.new(self.url)
     github_fork.get_forks.each do |fork|
       student = Student.find_or_create(:github_username => fork[:github_username])
+      student.update(:avatar_url => fork[:avatar_url])
       unless AssignmentSubmission.find(:student => student, :assignment => self)
         self.add_assignment_submission(:url => fork[:url], :student => student)
       end
